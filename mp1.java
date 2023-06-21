fe7ffe857a80f9b57d1a0272193c6d26ff3592ba
EinGitHub <137085448+EinGitHub@users.noreply.github.com>
2023-06-20 22:04:28 -0400

    First update

---
+++ mp1.java
@@ -0,0 +1,27 @@
+package Class1;
+
+public class mp1{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("what ever i am writing here");
+		System.out.println("pargol is the teacher " + "  and this is text i am aadding ");
+		System.out.print("Pargol is teaching java     ");
+		/*
+		this is my commnet ,msnas
+		i have many commnet
+		i have many commnet
+		i have many commnet
+		i have many commnet
+		i have many commnet
+		i have many commnet  
+		*/
+		/*
+		 6 + x = y
+		 */
+		
+		System.out.print("");
+		
+	}
+
+}
---
+++ mp3.java
@@ -0,0 +1,26 @@
+package Class1;
+
+public class mp3 {
+
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	int x = 6;
+	int y = 15;
+	int w = 33;
+	int z = x + y + w;
+	System.out.println(z);
+	System.out.println(x + w);
+	
+	
+	System.out.println("now try string");
+	String a = "6";
+	String b = "15";
+	String c = a + b;
+	System.out.println(c);
+	
+	
+	
+}
+
+}
